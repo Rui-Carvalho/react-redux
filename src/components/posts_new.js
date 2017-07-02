864f57880cb892dcf415189bb9ac873976e03fa8
Rui Carvalho <ruionline@yahoo.com>
2017-07-02 23:47:31 +0100

    Adding new component for new posts + finishing the reducer for list of posts

---
+++ src/components/posts_new.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { fetchPosts } from '../actions';
+// import _ from 'lodash';
+
+
+class PostsNew extends Component {
+
+    // componentDidMount() {
+    //     this.props.fetchPosts();
+    // }
+
+    render() {
+        return (
+            <div>
+                <h3>New Post</h3>
+            </div>
+        );
+    }
+}
+
+// function mapStateToProps(state) {
+//     return { posts: state.posts };
+// }
+//
+// export default connect(mapStateToProps, {fetchPosts} )(PostsIndex);
+export default PostsNew;
