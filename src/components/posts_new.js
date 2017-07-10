d3c2660ea21f23dcb9cfa147f47b86e2481c6e66
Rui Carvalho <ruionline@yahoo.com>
2017-07-10 01:11:56 +0100

    Using an anchor link (component) to save and cancel + plugging form submission + handling styling for form errors + wiring redux on top of redux-form + using history.push to do programatic navigation after new post is created

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
     renderField(field) {
+        const {meta: {touched, error} } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text" {...field.input}
                 />
+                <div className="text-help">
+                    { touched ? error : '' }
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        //console.log(values);
+        this.props.history.push('/'); //==> navigates to the list of posts
+        this.props.createPost(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Post Title"
                     name="title"
@@ -33,6 +50,8 @@ class PostsNew extends Component {
                     name="content"
                     component={ this.renderField }
                 />
+                <button type="submit" className="btn btn-primary">Save</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -63,5 +82,7 @@ function validate(values) {
 
 export default reduxForm({
     validate,
-    form: 'PostsNewForm'
-})(PostsNew);
+    form: 'PostsNewForm' //This is a unique name given to the form
+})(
+    connect(null,{ createPost })(PostsNew) //This returns a react component - now a container - passed to reduxForm
+);
