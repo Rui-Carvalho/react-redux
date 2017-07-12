237ed2fd05aef551f85d0f6b048c6b2a7576f339
Rui Carvalho <ruionline@yahoo.com>
2017-07-12 11:12:07 +0100

    Adding a safe check for post before it loads async on ajax (loading...) + Use post already fetched if available on posts show

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
 
     componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.fetchPost(id);
+        if(!this.props.post) { //Using the already fetched post if it is available
+            const { id } = this.props.match.params;
+            this.props.fetchPost(id);
+        }
     }
 
     render() {
         const { post } = this.props;
 
+        if(!post) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
+                <Link to="/" className="btn btn-secondary">Back</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
